f5a5b8a99bc6341b6dfd59edea0a573b8c21ee2b
ashsajal1 <ashsajal1@gmail.com>
2024-01-20 13:50:34 +0800

    create outine button and custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import { blue, gray, slate, violet } from 'tailwindcss/colors';
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: violet[600],
+        primaryHigh: violet[700],
+        secondary: blue[600],
+        light: slate[50],
+        dark: gray[700],
+      },
+    },
   },
   plugins: [],
-}
+};
