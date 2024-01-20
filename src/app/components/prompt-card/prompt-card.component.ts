3b6cf84901ba30ecea1d7d7873044e17aa44d972
ashsajal1 <ashsajal1@gmail.com>
2024-01-20 14:51:07 +0800

    Add handleCopy function

--- src/app/components/prompt-card/prompt-card.component.ts
+++ src/app/components/prompt-card/prompt-card.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-prompt-card',
   templateUrl: './prompt-card.component.html',
-  styleUrls: ['./prompt-card.component.scss']
+  styleUrls: ['./prompt-card.component.scss'],
 })
 export class PromptCardComponent {
+  copied: boolean = false;
+  promtText: string = 'This is prompt text';
 
+  handleCopy() {
+    this.copied = true;
+    // console.log(this.copied)
+
+    setTimeout(() => {
+      this.copied = false;
+      // console.log(this.copied)
+    }, 1000);
+  }
 }
