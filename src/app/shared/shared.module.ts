cbf1cc566d720589822ee0a6be2562de9e279ac2
ashsajal1 <ashsajal1@gmail.com>
2024-01-21 17:51:26 +0800

    generate footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    HeroComponent
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
