8c55fdf2c5a8cddc2d730d53289bd7453ef6009b
ashsajal1 <ashsajal1@gmail.com>
2024-01-22 18:16:53 +0800

    add signin and signup route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes, provideRouter } from '@angular/router';
 import { HomeComponent } from './shared/pages/home/home.component';
+import { SigninComponent } from './shared/pages/signin/signin.component';
+import { SignupComponent } from './shared/pages/signup/signup.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  // { path: 'signup', component: SignupComponent },
-  // { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: SigninComponent },
 ];
